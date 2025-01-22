55148521f40a0213999880475838bdfc26385b05
Victor Veldstra <victor.veldstra@gmail.com>
2025-01-22 17:00:29 +0100

    Fix ensure firing during EndPlay

--- Source/ALS/Private/AlsCharacter_Actions.cpp
+++ Source/ALS/Private/AlsCharacter_Actions.cpp
@@ -680,10 +680,12 @@ void AAlsCharacter::OnMantlingEnded_Implementation() {}
 bool AAlsCharacter::IsRagdollingAllowedToStart() const
 {
 	return LocomotionAction != AlsLocomotionActionTags::Ragdolling &&
-	       ALS_ENSURE_MESSAGE(GetMesh()->GetBodyInstance(UAlsConstants::PelvisBoneName()) != nullptr &&
-	                          GetMesh()->GetBodyInstance(UAlsConstants::Spine03BoneName()) != nullptr,
-	                          TEXT("A physics asset with the %s and %s bones are required for the ragdolling to work."),
-	                          *UAlsConstants::PelvisBoneName().ToString(), *UAlsConstants::Spine03BoneName().ToString());
+		GetWorld() &&
+		!GetWorld()->bIsTearingDown && //If you have a latent blueprint PlayMontage node, into a StartRagdoll node, the BP montage gets aborted on EndPlay, resulting in a false negative ensure going off
+	    ALS_ENSURE_MESSAGE(	GetMesh()->GetBodyInstance(UAlsConstants::PelvisBoneName()) != nullptr &&
+							GetMesh()->GetBodyInstance(UAlsConstants::Spine03BoneName()) != nullptr,
+							TEXT("A physics asset with the %s and %s bones are required for the ragdolling to work."),
+							*UAlsConstants::PelvisBoneName().ToString(), *UAlsConstants::Spine03BoneName().ToString());
 }
 
 void AAlsCharacter::StartRagdolling()
