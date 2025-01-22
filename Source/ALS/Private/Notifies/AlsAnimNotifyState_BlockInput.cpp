ad63fa4477dd5d95a1d04457773c202fa9daf724
Victor Veldstra <victor.veldstra@gmail.com>
2025-01-22 18:17:45 +0100

    Also block jumping while the BlockInput animation notify state is active

--- Source/ALS/Private/Notifies/AlsAnimNotifyState_BlockInput.cpp
+++ Source/ALS/Private/Notifies/AlsAnimNotifyState_BlockInput.cpp
@@ -2,10 +2,18 @@
 #include "Notifies/AlsAnimNotifyState_BlockInput.h"
 
 // Engine
+#include "NativeGameplayTags.h"
 
 // Plugin
 #include "AlsCharacter.h"
 #include "AlsCharacterMovementComponent.h"
+
+namespace AlsLocomotionActionTags
+{
+	// Since AAlsCharacter::Jump checks whether we are not doing any locomotion action, simply adding one for the purpose
+	// of blocking jumps will suffice.
+	UE_DEFINE_GAMEPLAY_TAG_STATIC(BlockJump, FName{ TEXTVIEW("Als.LocomotionAction.BlockJump") });
+}
 
 UAlsAnimNotifyState_BlockInput::UAlsAnimNotifyState_BlockInput()
 {
@@ -29,6 +37,7 @@ void UAlsAnimNotifyState_BlockInput::NotifyBegin(USkeletalMeshComponent* Mesh, U
 		UAlsCharacterMovementComponent* ALSCharacterMovementComponent = Cast<UAlsCharacterMovementComponent>(Character->GetMovementComponent());
 		if (IsValid(ALSCharacterMovementComponent))
 		{
+			Character->SetLocomotionAction(AlsLocomotionActionTags::BlockJump);
 			ALSCharacterMovementComponent->SetInputBlocked(true);
 		}
 	}
@@ -44,6 +53,7 @@ void UAlsAnimNotifyState_BlockInput::NotifyEnd(USkeletalMeshComponent* Mesh, UAn
 		UAlsCharacterMovementComponent* ALSCharacterMovementComponent = Cast<UAlsCharacterMovementComponent>(Character->GetMovementComponent());
 		if (IsValid(ALSCharacterMovementComponent))
 		{
+			Character->SetLocomotionAction(FGameplayTag::EmptyTag);
 			ALSCharacterMovementComponent->SetInputBlocked(false);
 		}
 	}
